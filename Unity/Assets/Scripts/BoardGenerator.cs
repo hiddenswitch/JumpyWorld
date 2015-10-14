dac5ee2476337fe2261d0f68e201eb2b6937d6e0
maschka <mashkarel@gmail.com>
2015-10-14 18:06:47 -0400

    level gradient and water
    Added water boxes, and difficulty gradient

--- Unity/Assets/Scripts/BoardGenerator.cs
+++ Unity/Assets/Scripts/BoardGenerator.cs
@@ -6,10 +6,14 @@ namespace JumpyWorld
 {
     public class BoardGenerator : MonoBehaviour
     {
-		public GameObject groundBox;
-		public int columns;
-		public int rows;
-		public int maxHeight=3;
+		public GameObject[] groundBox;
+		public int columns=30;
+		public int rows=30;
+		public int columnsPerLevel=3;
+		public int level=1;
+		public GameObject[] dangers;
+		public GameObject[] walls;
+
 		private Transform boardHolder;
         // Use this for initialization
         void Start()
@@ -28,17 +32,48 @@ namespace JumpyWorld
         }
 		void BoardSetup()
 		{
+			int height;
 			boardHolder= new GameObject("Board").transform;
 			for (int x=-1; x<columns;x++)
 			{
 				for (int z=-1; z<rows;z++)
 				{
-					for (int y =-1 ; y<Random.Range(-1,maxHeight);y++){
-						print(Random.Range(-1,5));
-						GameObject instance= Instantiate (groundBox, new Vector3(x,y,z),Quaternion.identity)as GameObject;
-						instance.transform.SetParent(boardHolder);
+					GameObject[] objectType= groundBox;
+					level= (int)(Math.Max(x,z)/columnsPerLevel);
+
+					if(Random.Range (0,50)<2*level){
+						height=1;
 					}
+					else if(Random.Range (0,50)<level){
+						height=-1;						
+					}
+					else{
+						if(Random.Range (0,50)<level){
+							objectType=dangers;
+						}
+						height=0;
+					}
+					DrawTerrain(height,objectType,new Vector3(x,height,z));
+				
 				}
+			}
+		}
+		void LayoutObject(int Density, GameObject[] objects,int x, int row){
+
+		}
+		void addDangers(){
+
+		}
+		void DrawTerrain(int height, GameObject[] type, Vector3 loc){
+
+			if (height!=-1){
+				for (int y =-1; y< height-1; y++) {
+					GameObject ground=Instantiate (groundBox[0], loc, Quaternion.identity) as GameObject;
+					ground.transform.SetParent (boardHolder);
+				}
+				GameObject toInstantiate = type [Random.Range (0, type.Length-1)];
+				GameObject instance = Instantiate (toInstantiate, loc, Quaternion.identity) as GameObject;
+				instance.transform.SetParent (boardHolder);
 			}
 		}
     }
