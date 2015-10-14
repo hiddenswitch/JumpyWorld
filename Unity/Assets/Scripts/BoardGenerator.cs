3cb9715c88b32c38861a00814dca528ab57d907a
maschka <mashkarel@gmail.com>
2015-10-14 16:09:01 -0400

    Working on level generation

---
+++ Unity/Assets/Scripts/BoardGenerator.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic;
+using Random = UnityEngine.Random;
+namespace JumpyWorld
+{
+    public class BoardGenerator : MonoBehaviour
+    {
+		public GameObject groundBox;
+		public int columns;
+		public int rows;
+		public int maxHeight=3;
+		private Transform boardHolder;
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+        public void setupScene()
+        {
+			BoardSetup ();
+        }
+		void BoardSetup()
+		{
+			boardHolder= new GameObject("Board").transform;
+			for (int x=-1; x<columns;x++)
+			{
+				for (int z=-1; z<rows;z++)
+				{
+					for (int y =-1 ; y<Random.Range(-1,maxHeight);y++){
+						print(Random.Range(-1,5));
+						GameObject instance= Instantiate (groundBox, new Vector3(x,y,z),Quaternion.identity)as GameObject;
+						instance.transform.SetParent(boardHolder);
+					}
+				}
+			}
+		}
+    }
+}
---
+++ Unity/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace JumpyWorld {
+    public class GameManager : MonoBehaviour {
+        public BoardGenerator boardScript;
+        // Use this for initialization
+        void Awake() {
+            boardScript = GetComponent<BoardGenerator>();
+            InitGame();
+        }
+        void InitGame()
+        {
+            boardScript.setupScene();
+        }
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
