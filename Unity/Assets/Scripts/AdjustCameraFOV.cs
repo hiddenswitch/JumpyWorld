f7ae4bb986872ba070f7a60b69147c5d0750cf0d
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2015-10-23 15:59:39 -0400

    Fixing camera for portrait vs. landscape

---
+++ Unity/Assets/Scripts/AdjustCameraFOV.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace JumpyWorld
+{
+	[ExecuteInEditMode()]
+	public class AdjustCameraFOV : MonoBehaviour
+	{
+		public float landscapeFOV;
+		public float portraitFOV;
+		public Camera camera;
+		float previousFOV;
+		// Use this for initialization
+		void Start ()
+		{
+			camera = camera ?? GetComponent<Camera> () ?? Camera.main;
+			previousFOV = camera.fieldOfView;
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+#if UNITY_EDITOR
+			if (Screen.width > Screen.height) {
+				if (previousFOV != landscapeFOV) {
+					camera.fieldOfView = previousFOV = landscapeFOV;
+				}
+			} else {
+				if (previousFOV != portraitFOV) {
+					camera.fieldOfView = previousFOV = portraitFOV;
+				}
+			}
+#elif
+			switch (Screen.orientation) {
+			case ScreenOrientation.LandscapeLeft:
+			case ScreenOrientation.LandscapeRight:
+				if (previousFOV != landscapeFOV) {
+					camera.fieldOfView = previousFOV = landscapeFOV;
+				}
+				break;
+			default:
+				if (previousFOV != portraitFOV) {
+					camera.fieldOfView = previousFOV = portraitFOV;
+				}
+				break;
+			}
+#endif
+		}
+	}
+
+}
