a275da20da89f091aa9067ab5aa51d504fc5a452
maschka <mashkarel@gmail.com>
2015-11-03 19:46:16 -0500

    pertrube path should work
    it may have a small error at the end, it may be something as an off by
    one error

--- Unity/Assets/Scripts/BoardGenerator.cs
+++ Unity/Assets/Scripts/BoardGenerator.cs
@@ -35,7 +35,7 @@ namespace JumpyWorld
     
 //		private List<Vector3> gridPositions = new List<Vector3> ();
 		private List<Vector3> pathPositions = new List<Vector3> ();
-        private List<Vector3> visitedPositions = new List<Vector3> ();
+        private List<Vector3> pathBackBone = new List<Vector3> ();
         private Vector3 lastDirection=new Vector3(0,0,0);
 		// Use this for initialization
 
@@ -87,27 +87,41 @@ namespace JumpyWorld
 		{
 			// TODO: Create an exciting path between two points
             //Bresenham's algorithm
-            pathPositions=(StraighPath(startpoint,endpoint));
-            for (int i=0; i < 15; i++) {
-                PertrubePath(pathPositions);
+
+           pathBackBone=(StraighPath(startpoint,endpoint,false,5));
+            //pathPositions=(StraighPath(new Vector3(0,0,0), new Vector3(5,0,13), true, 1));
+            for (int i=0; i < 10; i++) {
+               PertrubePath(pathBackBone);
             }
+            for (int i =1; i < pathBackBone.Count; i++) {
+                pathPositions.AddRange(StraighPath(pathBackBone[i-1],pathBackBone[i],true,1));
+            }
+
             for (int i =0; i< pathPositions.Count; i++) {
                 DrawTerrain(0,groundBox,pathPositions[i]);
             }
 		}
-        List<Vector3> StraighPath(Vector3 startpt, Vector3 endpt){
+        List<Vector3> StraighPath(Vector3 startpt, Vector3 endpt, Boolean pathable, int density){
             List<Vector3>path= new List<Vector3>();
             Vector3 start=startpt;
             Vector3 end=endpt;
             Vector3 directionLine= endpt - startpt;
-            float m = directionLine [2] / directionLine [0];
+            float m = (float)directionLine [2] / (float)directionLine [0];
+
             int z = (int)startpt [2];
             float epsilon = m - 1.0f;
+            print ("what"+epsilon);
+
             int drivingAxis=0;
             int otherAxis = 2;
             if (Math.Abs(directionLine [2]) > Math.Abs (directionLine [0])){
                 drivingAxis=2;
                 otherAxis=0;
+                print ("changgeee");
+                print (epsilon);
+                epsilon=(1.0f/m)-1;
+                m=1.0f/m;
+                print (epsilon);
             }
             if (startpt [drivingAxis] > endpt [drivingAxis]) {
                 start= endpt;
@@ -116,16 +130,23 @@ namespace JumpyWorld
             Vector3 currentPoint = start;
             Vector3 connectingPoint = new Vector3 (0, 0, 0);
             connectingPoint[drivingAxis]=1;
-            for (int i = (int)start[drivingAxis]; i<end[drivingAxis];i=i+1){
+
+            for (int i = (int)start[drivingAxis]; i<=end[drivingAxis];i=i+1){
                 currentPoint[drivingAxis]=i;
+                if (i%density==0){
                 path.Add(currentPoint);
-                path.Add (currentPoint+connectingPoint);
-                if (epsilon>=1.0){
+                if (pathable && !path.Contains(currentPoint+connectingPoint)){
+                        path.Add (currentPoint+connectingPoint);}}
+                if (epsilon>1.0){
                     currentPoint[otherAxis]+=1;
                     epsilon-=1.0f;
                 }
                 epsilon+=m;
             }
+            if (!path.Contains(end)){
+                path.Add(end);
+            }
+
             return path;
         }
 		void CreateRoom ()
@@ -160,59 +181,48 @@ namespace JumpyWorld
 		}
         void PertrubePath(List<Vector3> path){
             var dir = new List<Vector3>{new Vector3(0,0,2), new Vector3(0,0,-2),new Vector3(2,0,0),new Vector3(-2,0,0)};
-            int index =  Random.Range (2, (path.Count-2));
+            int index =  Random.Range (1, (path.Count-1));
             Vector3 movePoint = path [index];
-           //DrawTerrain (0, dangers, movePoint);
-            movePoint += dir [Random.Range (0, dir.Count)];
-            List<Vector3> modifiedStart = StraighPath (path [index - 2], movePoint);
-            List<Vector3> modifiedEnd= StraighPath(movePoint,path[index+2]);
-            path.RemoveAt (index);
-            path.RemoveAt (index - 1);
-            path.RemoveAt (index+1);
-            for (int i=0; i<modifiedStart.Count; i++) {
-                path.Insert(i+index-1,modifiedStart[i]);
-            }
-            for (int i=0; i <modifiedEnd.Count; i++ ){
-                path.Insert(i+index+modifiedStart.Count,modifiedEnd[i]);
-            }
+            path [index] = movePoint + dir [Random.Range (0, dir.Count)];
         }
 		Vector3 PickNeighbor (Vector3 pt,int e)
 		{
-            int index;
-            int indexOfLast;
-            var dir = new List<Vector3>{new Vector3(0,0,1), new Vector3(0,0,-1),new Vector3(1,0,0),new Vector3(-1,0,0)};
-			if (pt [2] >= rows - 1) {
-				dir.Remove (new Vector3(0,0,1));
-			} else if (pt [2] <= 0) {
-				dir.Remove (new Vector3(0,0,-1));
-			}
-			if (pt [0] >= columns - 1) {
-                dir.Remove (new Vector3 (1, 0, 0));
-            } else if (pt [0] <= 0) {
-                dir.Remove(new Vector3(-1,0,0));             
-               }
-            if (dir.Contains (lastDirection)) {
-                indexOfLast = dir.IndexOf (lastDirection);
-                index = Random.Range (0, dir.Count + e);
-                if (index >= dir.Count) {
-                    index = indexOfLast;
-                }
-            } else {
-                index = Random.Range (0, dir.Count);
-            }
-			Vector3 newpt = pt;
-            print (dir.Count);
-            print (index);
-			while (visitedPositions.Contains(newpt)&& dir.Count>0) {
-                newpt = pt + dir [index];
-                lastDirection = dir [index];
-                dir.RemoveAt (index);
-                index = Random.Range (0, dir.Count);   
-                print ("this is the index: "+ index);
-                print ("this is the dir"+ dir.Count);
-               				
-			}
-			return newpt;
+//            int index;
+//            int indexOfLast;
+//            var dir = new List<Vector3>{new Vector3(0,0,1), new Vector3(0,0,-1),new Vector3(1,0,0),new Vector3(-1,0,0)};
+//			if (pt [2] >= rows - 1) {
+//				dir.Remove (new Vector3(0,0,1));
+//			} else if (pt [2] <= 0) {
+//				dir.Remove (new Vector3(0,0,-1));
+//			}
+//			if (pt [0] >= columns - 1) {
+//                dir.Remove (new Vector3 (1, 0, 0));
+//            } else if (pt [0] <= 0) {
+//                dir.Remove(new Vector3(-1,0,0));             
+//               }
+//            if (dir.Contains (lastDirection)) {
+//                indexOfLast = dir.IndexOf (lastDirection);
+//                index = Random.Range (0, dir.Count + e);
+//                if (index >= dir.Count) {
+//                    index = indexOfLast;
+//                }
+//            } else {
+//                index = Random.Range (0, dir.Count);
+//            }
+//			Vector3 newpt = pt;
+//            print (dir.Count);
+//            print (index);
+//			while (visitedPositions.Contains(newpt)&& dir.Count>0) {
+//                newpt = pt + dir [index];
+//                lastDirection = dir [index];
+//                dir.RemoveAt (index);
+//                index = Random.Range (0, dir.Count);   
+//                print ("this is the index: "+ index);
+//                print ("this is the dir"+ dir.Count);
+//               				
+//			}
+//			return newpt;
+            return new Vector3 ();
 		}
 
 		void DrawTerrain (int height, GameObject[] type, Vector3 loc)
