68e94299daf8503e2365cc14841ff88f43cb672b
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2015-11-12 22:34:54 -0500

    Try to fix settings

---
+++ Unity/Assets/Scripts/SetFramerate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace JumpyWorld
+{
+	public class SetFramerate : MonoBehaviour
+	{
+		// Use this for initialization
+		void Awake ()
+		{
+			Application.targetFrameRate = 60;
+		}
+	}
+}
