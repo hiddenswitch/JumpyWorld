64a1f3c0002845b3fdc473206a9a915d8e99cd2e
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2015-11-15 21:52:11 -0500

    Randomly tweak the UV shells of the top of the cubes

---
+++ Unity/Assets/Scripts/Editor/LabelHandle.cs
@@ -0,0 +1,34 @@
+﻿using UnityEditor;
+using System.Collections;
+using UnityEngine;
+
+namespace JumpyWorld
+{
+	[CustomEditor(typeof(RandomizeUVShells))]    
+	public class LabelHandle : Editor
+	{
+		void OnSceneGUI ()
+		{            
+			Handles.BeginGUI (); 
+			Handles.color = Color.blue;
+		
+			RandomizeUVShells cubeController = (RandomizeUVShells)target;
+		
+			var mesh = cubeController.gameObject.GetComponentInChildren<MeshFilter> ().sharedMesh;
+			var vertices = mesh.vertices;
+			var triangles = mesh.triangles;
+			Debug.Log (triangles.Length);
+			Debug.Log (vertices.Length);
+
+			for (var i = 0; i < triangles.Length/3; i++) {
+				var p0 = vertices [triangles [i * 3]];
+				var p1 = vertices [triangles [i * 3 + 1]];
+				var p2 = vertices [triangles [i * 3 + 2]]; 
+				var planePoint = (1.0f / 3.0f) * (p0 + p1 + p2);
+				Handles.Label (planePoint, string.Format ("{0}, {1}, {2}", i * 3, i * 3 + 1, i * 3 + 2));
+			}
+		
+			Handles.EndGUI ();
+		}
+	}
+}
---
+++ Unity/Assets/Scripts/RandomizeUVShells.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace JumpyWorld
+{
+	public class RandomizeUVShells : MonoBehaviour
+	{
+		[System.Serializable]
+		public struct UVShellInfo
+		{
+			public int[] uvIndices;
+			public Rect location;
+		}
+		public MeshFilter meshFilter;
+		public bool randomizeOnStart;
+		public UVShellInfo[] shells;
+
+		public Vector2[] startUVs;
+		Mesh mesh;
+		// Use this for initialization
+		void Start ()
+		{
+			mesh = meshFilter.mesh;
+			startUVs = mesh.uv;
+			if (randomizeOnStart) {
+				Randomize ();
+			}
+		}
+
+		public void Randomize ()
+		{
+			Vector2[] newUVs = new Vector2[startUVs.Length];
+			startUVs.CopyTo (newUVs, 0);
+			for (var i = 0; i < shells.Length; i++) {
+				var shell = shells [i];
+				var displacement = Vector2.Lerp (shell.location.min, shell.location.max, Random.value) - startUVs [shell.uvIndices [0]];
+				for (var j = 0; j < shell.uvIndices.Length; j++) {
+					var k = shell.uvIndices [j];
+					newUVs [k] = startUVs [k] + displacement;
+				}
+			}
+			mesh.uv = newUVs;
+		}
+	}
+}
