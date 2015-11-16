1c1568bb2ad2c7aa254c9b5d1d93f106e6edcfb4
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2015-11-15 20:21:16 -0500

    Render money and collect it

---
+++ Unity/Assets/Scripts/RenderCollects.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System;
+
+namespace JumpyWorld
+{
+	public class RenderCollects : MonoBehaviour
+	{
+		public Text text;
+		public Collects collects;
+		public string formatter = "{0}";
+		// Update is called once per frame
+		void Update ()
+		{
+			text.text = String.Format (formatter, collects.count);
+		}
+	}
+}
