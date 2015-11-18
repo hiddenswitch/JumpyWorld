194421632d30a46e74c7c66c86d8a3fa5e2c05ad
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2015-11-17 22:15:26 -0500

    Fix issues with rebuilding world on death by explicitly reviving on death. Maybe you would need to dismiss a prompt to revive later, so make instant revival a script

---
+++ Unity/Assets/Scripts/InstantRevival.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace JumpyWorld
+{
+	public class InstantRevival : MonoBehaviour
+	{
+
+		// Use this for initialization
+		void OnObjectDied (GameObject sender)
+		{
+			var death = sender.GetComponent<DiesBelowDepth> ();
+			if (death == null) {
+				return;
+			}
+			death.ResetDies ();
+		}
+	}
+}
