dcea990f60cce96c76b2a3ef60daf1a2be0e607d
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2015-11-19 16:24:54 -0500

    Theme changing

---
+++ Unity/Assets/Scripts/Theme.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace JumpyWorld
+{
+	public class Theme : MonoBehaviour
+	{
+		public string name;
+		public TilePool tilePool;
+		public Material cameraBackground;
+	}
+}
---
+++ Unity/Assets/Scripts/ThemeChanger.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace JumpyWorld
+{
+	public class ThemeChanger : MonoBehaviour
+	{
+		public Theme[] themes;
+		public WorldBuilder worldBuilder;
+		public GameObject cameraBackground;
+
+		public void RandomizeTheme ()
+		{
+			var theme = themes [Random.Range (0, themes.Length)];
+			// TODO: This is just supporting the handful of things we actually want to do.
+			worldBuilder.tilePool = theme.tilePool;
+			cameraBackground.GetComponent<MeshRenderer> ().material = theme.cameraBackground;
+		}
+	}
+}
