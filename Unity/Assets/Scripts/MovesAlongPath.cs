7bf1b4cbd95896e557413de5676db8f755d679a3
Jennifer Wu <jennwu@mit.edu>
2015-11-20 14:59:14 -0500

    Ghost prefab with animation

--- Unity/Assets/Scripts/MovesAlongPath.cs
+++ Unity/Assets/Scripts/MovesAlongPath.cs
@@ -3,41 +3,64 @@ using System.Collections;
 
 namespace JumpyWorld
 {
-	[RequireComponent(typeof(CharacterController))]
+	[RequireComponent(typeof(Rigidbody))]
 	public class MovesAlongPath : MonoBehaviour
 	{
 		//<summary>
 		/// Path in world space
 		/// </summary>
 		public Vector3[] path;
-		CharacterController characterController;
+		Rigidbody rigidbody;
 		public bool shouldTeleportToStartOfPath;
 
 		// speed is > 0
 		public float speed;
 		private int pathIndex = -1;
 		private float arrivedThreshold;
+		private float height;
 
 		// Use this for initialization
 		void Start ()
 		{
-			this.characterController = this.GetComponent<CharacterController> ();
+			// check that speed is positive
+			if (speed <= 0) {
+				enabled = false;
+				Debug.LogError ("Speed needs to be positive");
+			}
+
+			// check if closed path
+			if (path [0] != path [path.Length - 1]) {
+				enabled = false;
+				Debug.LogError ("Not a closed path.");
+			}
+
+			// check if path is rectilinear
+			for (var i = 0; i < path.Length - 1; i++) {
+				var p0 = path [i];
+				var p1 = path [i + 1];
+				if (!isXZRectilinearLine (p0, p1)) {
+					enabled = false;
+					Debug.LogError ("Not rectilinear path.");
+				}
+			}
+
+			this.rigidbody = this.GetComponent<Rigidbody> ();
 			arrivedThreshold = speed / 2 * Time.fixedDeltaTime;
+			height = transform.position.y;
 			if (shouldTeleportToStartOfPath) {
 				pathIndex = 0;
 				transform.position = new Vector3 (path [0].x, transform.position.y, path [0].z);
-			} 
-			transform.forward = calculateForward (transform.position, path [0]);
-		}
-	
-		// Update is called once per frame
-		void Update ()
-		{
+			} else {
+				// check that the starting position is rectilinear to the starting path position
+				if (!isXZRectilinearLine (transform.position, path [0])) {
+					Debug.LogWarning ("Current position to start is not rectilinear.");
+				}
+			}
+			transform.forward = xzCalculateForward (transform.position, path [0]);
 		}
 
 		void FixedUpdate ()
 		{
-			var timeForFrame = Time.fixedDeltaTime;
 			var currentPosition = transform.position;
 			var nextPosition = path [(pathIndex + 1) % (path.Length - 1)];
 			var hasArrived = xzDistance (currentPosition, nextPosition) < arrivedThreshold;
@@ -45,23 +68,11 @@ namespace JumpyWorld
 				// move to the nextPosition, update forward direction
 				transform.position = new Vector3 (nextPosition.x, transform.position.y, nextPosition.z);
 				pathIndex = (pathIndex + 1) % (path.Length - 1);
-				transform.forward = calculateForward (nextPosition, path [(pathIndex + 1) % (path.Length - 1)]);
 			} 
+			transform.forward = xzCalculateForward (currentPosition, path [(pathIndex + 1) % (path.Length - 1)]);
 			// move the character forward
 			var velocity = transform.forward * speed;
-			characterController.Move (velocity * timeForFrame);
-		}
-
-		void OnDrawGizmos ()
-		{
-			Gizmos.color = Color.blue;
-			for (var i = 0; i < path.Length - 1; i++) {
-				var p0 = path [i];
-				p0.y += 0.5f;
-				var p1 = path [i + 1];
-				p1.y += 0.5f;
-				Gizmos.DrawLine (p0, p1);
-			}
+			rigidbody.velocity = velocity;
 		}
 
 		// Returns the distance between p1 and p2 ignoring the y component
@@ -73,9 +84,21 @@ namespace JumpyWorld
 		}
 
 		// Returns the forward vector corresponding to starting in p1 and ending at p2
-		Vector3 calculateForward (Vector3 start, Vector3 end)
+		// ignoring the y component
+		Vector3 xzCalculateForward (Vector3 start, Vector3 end)
 		{
-			return (end - start).normalized;
+			var newStart = new Vector3 (start.x, start.y, start.z);
+			var newEnd = new Vector3 (end.x, height, end.z);
+			return (newEnd - newStart).normalized;
+		}
+
+		// Returns true if start and end make a rectilinear line on the xz plane
+		bool isXZRectilinearLine (Vector3 start, Vector3 end)
+		{
+			if (start.x != end.x) {
+				return start.z == end.z;
+			}
+			return true;
 		}
 	}
 }
