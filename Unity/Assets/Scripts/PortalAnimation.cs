895808dd4a6f65f7a2bf49729c13b8f414f68f0b
Benjamin S. Berman <benjamin.s.berman@gmail.com>
2016-07-27 16:11:57 -0700

    Polished up Jumpy World build. Re-enabled portals, fixed spikes, changed appearance of player, coins, spikes and ghosts, introduced nice background texture

--- Unity/Assets/Scripts/PortalAnimation.cs
+++ Unity/Assets/Scripts/PortalAnimation.cs
@@ -1,47 +1,54 @@
 ﻿using UnityEngine;
 using System.Collections;
-namespace JumpyWorld { 
-    [RequireComponent (typeof (Rigidbody))]
-    public class PortalAnimation : MonoBehaviour {
+
+namespace JumpyWorld
+{
+	[RequireComponent (typeof(Rigidbody))]
+	public class PortalAnimation : MonoBehaviour
+	{
 
 
-        public float elevationTime;
-        public AnimationCurve elevationVelocityCurve;
-        public ClearAndRebuildWorld rebuild;
-        Rigidbody rb;
+		public float elevationTime;
+		public AnimationCurve elevationVelocityCurve;
+		public ClearAndRebuildWorld rebuild;
+		Rigidbody rb;
 
-	    // Use this for initialization
-	    void Start () {
-            rb = GetComponent<Rigidbody>();
-	    }
+		// Use this for initialization
+		void Start ()
+		{
+			rb = GetComponent<Rigidbody> ();
+			if (rebuild == null) {
+				rebuild = GameObject.FindObjectOfType<ClearAndRebuildWorld> ();
+			}
+		}
 	
-	    // Update is called once per frame
-	    void Update () {
+		// Update is called once per frame
+		void Update ()
+		{
 	
-	    }
+		}
 
-        void StartPortalAnimation()
-        {
-            StartCoroutine(PortalAnimationCoroutine());
-        }
+		void StartPortalAnimation ()
+		{
+			StartCoroutine (PortalAnimationCoroutine ());
+		}
 
-        IEnumerator PortalAnimationCoroutine()
-        {
-            rb.useGravity = false;
-            rb.isKinematic = true;
-            float time = Time.time;
-            while (Time.time - time < elevationTime)
-            {
+		IEnumerator PortalAnimationCoroutine ()
+		{
+			rb.useGravity = false;
+			rb.isKinematic = true;
+			float time = Time.time;
+			while (Time.time - time < elevationTime) {
 
-                gameObject.transform.position += elevationVelocityCurve.Evaluate((Time.time - time)/ elevationTime) * gameObject.transform.up * Time.deltaTime;
-                yield return new WaitForEndOfFrame();
-            }
+				gameObject.transform.position += elevationVelocityCurve.Evaluate ((Time.time - time) / elevationTime) * gameObject.transform.up * Time.deltaTime;
+				yield return new WaitForEndOfFrame ();
+			}
 
-            rebuild.RandomizeAndRebuild();
-            BroadcastMessage("ResetPosition");
+			rebuild.RandomizeAndRebuild ();
+			BroadcastMessage ("ResetPosition");
 
-            rb.useGravity = true;
-            rb.isKinematic = false;
-        }
-    }
+			rb.useGravity = true;
+			rb.isKinematic = false;
+		}
+	}
 }
